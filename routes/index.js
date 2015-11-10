435d2532be5df5400dad91ce9dc4ae7fcb72e6ec
Severin Ryberg <SevBerg129@Gmail.com>
2015-11-10 14:06:06 -0700

    removed default bootstrap navbar and added a collapsable sidebar

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    
-    req.sevdenDB.query("SELECT * FROM users", function(err, rows){
-        if(err) throw err;
-        console.log("\nwerk?");
-        console.log(rows);
-    });
-    
+
     res.render('index', { title: 'Express' });
+
 });
 
 module.exports = router;
